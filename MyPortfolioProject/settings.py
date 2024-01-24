4a054c81e8c2ef65bf092a622413f2a0d6fa040b
rarell202 <rafael.arellanes1@gmail.com>
2024-01-24 10:13:03 -0600

    Updated Django to 5.0.1

--- MyPortfolioProject/settings.py
+++ MyPortfolioProject/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
 ]
 
 ROOT_URLCONF = 'MyPortfolioProject.urls'
