7ec3dafcd3d9cfa8e7c3326fe1b0f6c8e23b3afc
rarell202 <rafael.arellanes1@gmail.com>
2024-01-25 10:41:27 -0600

    Installed Whitenoise and added necessary lines.

--- MyPortfolioProject/settings.py
+++ MyPortfolioProject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,12 @@ STATICFILES_DIRS = [
 
 # The directory where Django will collect static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # You can adjust the path as needed
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
