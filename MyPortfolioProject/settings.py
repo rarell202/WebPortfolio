3249c611efba790d3b3d63217c9340d05ecef369
rarell202 <rafael.arellanes1@gmail.com>
2024-01-25 11:24:27 -0600

    Added Configuring SSL to Settings.py

--- MyPortfolioProject/settings.py
+++ MyPortfolioProject/settings.py
@@ -137,3 +137,13 @@ STORAGES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+     SECURE_SSL_REDIRECT = False
+     SESSION_COOKIE_SECURE = False
+     CSRF_COOKIE_SECURE = False
+else:
+     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+     SECURE_SSL_REDIRECT = True
+     SESSION_COOKIE_SECURE = True
+     CSRF_COOKIE_SECURE = True
