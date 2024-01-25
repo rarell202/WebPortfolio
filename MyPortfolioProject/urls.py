d32b1af2dc73b7c1632810cf66fbddebcbb31c4d
rarell202 <rafael.arellanes1@gmail.com>
2024-01-25 09:47:44 -0600

    Changed Settings and url, and fixed title on html page.

--- MyPortfolioProject/urls.py
+++ MyPortfolioProject/urls.py
@@ -23,6 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
